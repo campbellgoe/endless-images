48d1a2d7a7c886544adb9c59599c8d6b86919ad8
GCDeveloper <campbell.goe@gmail.com>
2019-02-12 10:06:08 +0000

    basic page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+class Image extends Component {
+  render(){
+    return (
+      <article className="image-article" style={this.props.style}>
+        <img src={`https://picsum.photos/${this.props.size}`} alt="random"/>
+      </article>
+    );
+  }
+}
 class App extends Component {
+  constructor(){
+    super();
+    this.width = window.innerWidth;
+    this.height = window.innerHeight;
+    this.size = Math.floor(this.width*0.5);
+  }
+  componentDidMount(){
+
+    console.log("this.width:", this.width);
+  }
+  createImages(baseSize, startId, n){
+    let images = [];
+    for(let i=0;i < n;i++){
+      images.push(<Image
+        key={"img-"+i}
+        size={baseSize+i+startId}
+        style={{
+          width: baseSize+"px",
+          height: baseSize+"px"
+        }}/>);
+    }
+    return images;
+  }
   render() {
+
+    const images = this.createImages(this.size, 0, 3);
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          Welcome to Endless
         </header>
+        {images}
       </div>
     );
   }
